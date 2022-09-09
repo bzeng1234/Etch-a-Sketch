00fa16e0ffb5ac9f4aac12e6a1afba2d09d09dc3
Brandon Zeng <bzeng1234@gmail.com>
2022-09-08 18:40:06 -0700

    Extra credit - Add random RGB value

--- script.js
+++ script.js
@@ -28,8 +28,12 @@ function addEvent() {
 
     columnBlockArr.forEach((column) => {
         column.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'red';
-    });
+            const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+            const r = randomBetween(0, 255);
+            const g = randomBetween(0, 255);
+            const b = randomBetween(0, 255);
+            e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+        });
     });
 }
 
